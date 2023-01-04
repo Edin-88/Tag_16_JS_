cde02329857473c49deca46e7c74d4f1ad89d04e
Your Name <selmanovic.edin1@gmail.com>
2023-01-04 15:40:34 +0100

    finished_2

--- comparisons_lev_1__15_/assets/js/main.js
+++ comparisons_lev_1__15_/assets/js/main.js
@@ -1,12 +1,12 @@
-let number = 5
+let num = 5
 let anotherNumber = 8
 
-console.log(5 == 8);
-console.log(5 > 8);
-console.log(5 < 8);
-console.log(5 >= 8);
-console.log(5 <= 8);
+console.log(num == 8);
+console.log(mum > 8);
+console.log(num < 8);
+console.log(num >= 8);
+console.log(num <= 8);
 
-console.log(5 == "5");
-console.log(5 != 8);
-console.log(5 != "5")
+console.log(num == "5");
+console.log(num != 8);
+console.log(num != "5")
