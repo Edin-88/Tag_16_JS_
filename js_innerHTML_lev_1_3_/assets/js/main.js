f0934d43c86869e917820aa63f9975eba51898da
Your Name <selmanovic.edin1@gmail.com>
2023-01-04 15:35:33 +0100

    finished

---
+++ js_innerHTML_lev_1_3_/assets/js/main.js
@@ -0,0 +1,7 @@
+let gallery = document.getElementById("gallery");
+
+
+gallery.innerHTML += "<figure><img src= assets/img/pic_1.avif> <p>fig1</p> </figure>" 
+gallery.innerHTML += "<figure><img src= assets/img/pic_1.avif> <p>fig2</p> </figure>" 
+gallery.innerHTML += "<figure><img src= assets/img/pic_1.avif> <p>fig3</p> </figure>" 
+
